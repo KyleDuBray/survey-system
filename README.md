cf371b7efb2db4639b29af7218880a3b62b6c673
Kyle DuBray <krdubray@comcast.net>
2023-04-18 14:33:46 -0500

    add initial .env initialization and database connection

--- README.md
+++ README.md
@@ -5,3 +5,15 @@ A web-based survey system created with HTML, CSS, JavaScript, and PHP.
 ## Setup
 
 [Composer](https://getcomposer.org/) is used for package management. Once installed, run`composer install` in the root project directory to fetch dependencies.
+
+Add the following key/value pairs to your .env file at the project root:
+
+```sh
+
+DB_HOST = host_ip_for_your_database
+DB_PORT = port_running_mysql
+DB_USER = your_db_username
+DB_NAME = your_database_name (if going by the schema, will be "survey_system")
+DB_PASSWORD = your_database_password
+
+```
--- php/index.php
+++ php/index.php
@@ -10,7 +10,31 @@
 
 <body>
     <?php
-    echo "Hello there";
+    require '../vendor/autoload.php';
+
+    // Get config from .env file
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . "/..");
+    $dotenv->load();
+    $DB_USER     = $_ENV['DB_USER'];
+    $DB_PASSWORD = $_ENV['DB_PASSWORD'];
+    $DB_HOST     = $_ENV['DB_HOST'];
+    $DB_PORT     = $_ENV['DB_PORT'];
+    $DB_NAME     = $_ENV['DB_NAME'];
+
+    echo "Hello there" . "<br>";
+
+
+    $host     = $DB_HOST . ":" . $DB_PORT;
+    $username = $DB_USER;
+    $password = $DB_PASSWORD;
+    $database = $DB_NAME;
+
+    // Create connection
+    $conn = new mysqli($host, $username, $password, $database);
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
     ?>
 </body>
 
