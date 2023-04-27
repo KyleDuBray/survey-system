b493a0cdbc4a43b6a77fbc78f2682e5ab85619ef
DuBrowski <krdubray@comcast.net>
2023-04-27 15:39:10 -0500

    restore original survey creation form

--- private/surveyForm.php
+++ private/surveyForm.php
@@ -29,7 +29,7 @@ if (!isset($_SESSION['loggedin'])) {
 		</div>
 	</nav>
 	<div class="content">
-		<form id="surveyForm" method="post" action="../includes/take-survey.inc.php">
+		<form id="surveyForm" action="">
 			<!-- tabs modded by sql question no -->
 			<!-- 2 current question types, mult choice & free response  -->
 			<?php
@@ -55,7 +55,7 @@ if (!isset($_SESSION['loggedin'])) {
 				//switch for response type
 				switch ($question_type) {
 					case 'free-response':
-						echo "<p><input placeholder='Please Type Response' name='answer[]' oninput='this.className = '''></p>";
+						echo "<p><input placeholder='Please Type Response' oninput='this.className = '''></p>";
 						break;
 					case "multiple-choice":
 						$sql = "SELECT COUNT(DISTINCT option_id) FROM question_option WHERE option_type = 'multiple-choice' AND question_id = '" . $arr[$i] . "'";
@@ -66,7 +66,7 @@ if (!isset($_SESSION['loggedin'])) {
 						for ($j = 0; $j < $option_limit; $j++) {
 							$sql = "SELECT option_text FROM question_option WHERE option_id = '" . $option_arr[$j] . "' AND question_id = '" . $arr[$i] . "'";
 							$option_text = getDataElement($conn, $sql);
-							echo "<input type='radio' name='answer[]' value='", $option_text, "' oninput='this.className = '''>", $option_text;
+							echo "<input type='radio' name =", $i, " value='", $option_text, "' oninput='this.className = '''>", $option_text;
 							//name may change from '$i' with backend
 						}
 						break;
