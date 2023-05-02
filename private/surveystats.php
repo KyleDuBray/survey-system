71cc950ff3a95181a1f193e8661ed2560930e51a
DuBrowski <krdubray@comcast.net>
2023-05-01 19:31:28 -0500

    add basic styles to survey stats

--- private/surveystats.php
+++ private/surveystats.php
@@ -1,3 +1,13 @@
+<?php
+// We need to use sessions, so you should always start sessions using the below code.
+session_start();
+// If the user is not logged in redirect to the login page...
+if (!isset($_SESSION['loggedin'])) {
+    header('Location: ../public/home.php');
+    exit;
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -14,16 +24,19 @@
     <?php
     include "../shared/navbar.php";
     require_once '../includes/dbc.inc.php';
-    session_start();
 
-    // If the user is not logged in redirect to the login page...
-    if (!isset($_SESSION['loggedin']) || !isset($_GET['survey_id'])) {
-        header("location: ../public/login.php");
-        exit;
-    }
+
+
     // get id of logged in user
     $user_id = $_SESSION['id'];
     $survey_id = $_GET['survey_id'];
+
+    $sql_get_title = "SELECT title FROM survey WHERE survey_id = " . $survey_id;
+    $titleRes = mysqli_query($conn, $sql_get_title);
+    $title = mysqli_fetch_assoc($titleRes);
+
+    echo "<h1>" . $title["title"] . " Results</h1>";
+    echo "<div class='stats-container'>";
 
     $sql_get_questions = "SELECT * FROM question
     INNER JOIN question_option on question.question_id = question_option.question_id
@@ -68,6 +81,7 @@
             while ($res = mysqli_fetch_assoc($responses)) {
                 echo "<li>" . $res['response_text'] . "</li>";
             }
+            echo "</ul>";
             $newQuestion = true;
         }
         // else if this is a multiple choice question
@@ -88,6 +102,9 @@
         }
         echo "<br>";
     }
+
+    echo "</div>";
+
 
     function getFreeResponses($conn, $survey_id, $question_id)
     {
